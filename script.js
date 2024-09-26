90649b15133231c1986020ff1b1bef3247cc162b
Huy Nguyen <huynguyen560@gmail.com>
2024-09-25 18:23:24 -0700

    feat: add winner checking logic - Game now is playable - Need to add styling
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- script.js
+++ script.js
@@ -1,49 +1,157 @@
-const gameBoard = (function Gameboard() {
-  let board = [
-    [".", ".", "."],
-    [".", ".", "."],
-    [".", ".", "."],
-  ];
-  const getBoard = () => board;
-  const setBoard = (row, col, mark) => {
-    if (row < 3 && row >= 0 && col < 3 && col >= 0) board[row][col] = mark;
-  };
-  return { getBoard, setBoard };
-})();
+const newGameButton = document.querySelector(".new-game-button");
+const boardDiv = document.querySelector(".board");
+const resultDiv = document.querySelector(".result");
 
-function createPlayer(player) {
-  this.player = player;
-  const getPlayer = () => this.player;
-  return { getPlayer };
+function createPlayer(mark, name) {
+  const getMark = () => mark;
+  const getName = () => name;
+  return { getMark, getName };
 }
 
-function game(player1, player2, board, turn) {
-  let winner = null;
+function createGame(player1Name, player2Name, board) {
+  let winner = { player: null, cells: [], message: "" };
 
-  const updateGame = (player1, player2, board, turn) => {
-    const row = 0;
-    const col = 0;
-    const mark = player1 === turn ? player1 : player2;
-    board.setBoard(row, col, mark);
+  const player1 = createPlayer("X", player1Name);
+  const player2 = createPlayer("O", player2Name);
 
-    // Set the winner
+  let currentPlayerTurn = player1;
+
+  const updateGame = (cell) => {
+    const cellId = cell.id;
+    const row = cellId.charAt(0);
+    const col = cellId.charAt(1);
+    if (board.getBoard()[row][col] === "." && winner.player === null) {
+      const mark =
+        player1 === currentPlayerTurn ? player1.getMark() : player2.getMark();
+
+      board.setBoard(row, col, mark);
+
+      cell.innerText = mark;
+
+      checkWinner(mark);
+
+      if (winner.player !== null) {
+        for (let i = 0; i < winner.cells.length; i++) {
+          // const winningCell = document.querySelector("#" + winner.cells[i]);
+          // winningCell.computedStyleMap.backgroundColor = "green";
+        }
+        resultDiv.innerText = winner.message;
+      } else {
+        currentPlayerTurn = player1 === currentPlayerTurn ? player2 : player1;
+      }
+    }
   };
+
+  function checkWinner(mark) {
+    // Check for tie
+    let isTie = true;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board.getBoard()[i][j] === ".") {
+          isTie = false;
+        }
+      }
+    }
+
+    if (isTie) {
+      winner.player = "tie";
+      winner.message = "Tie game!";
+    } else {
+      const winningMark = `${mark}${mark}${mark}`;
+
+      // Check all rows and update winner object is there's a winner
+      for (let i = 0; i < 3; i++) {
+        let currRowMark = "";
+        let rows = [];
+        for (let j = 0; j < 3; j++) {
+          currRowMark += board.getBoard()[i][j];
+          rows = [...rows, [`${i}${j}`]];
+        }
+        if (currRowMark === winningMark) {
+          winner.player = currentPlayerTurn;
+          winner.cells = rows;
+          winner.message = `${currentPlayerTurn.getName()} is the winner!`;
+        }
+      }
+
+      // Check all columns and update winner object if there's a winner
+      for (let i = 0; i < 3; i++) {
+        let currColMark = "";
+        let cols = [];
+        for (let j = 0; j < 3; j++) {
+          currColMark += board.getBoard()[j][i];
+          cols = [...cols, [`${i}${j}`]];
+        }
+        if (currColMark === winningMark) {
+          winner.player = currentPlayerTurn;
+          winner.cells = cols;
+          winner.message = `${currentPlayerTurn.getName()} is the winner!`;
+        }
+      }
+
+      // Check the two diagonals
+      if (
+        board.getBoard()[0][0] +
+          board.getBoard()[1][1] +
+          board.getBoard()[2][2] ===
+        winningMark
+      ) {
+        winner.player = currentPlayerTurn;
+        winner.cells = ["00", "11", "22"];
+        winner.message = `${currentPlayerTurn.getName()} is the winner!`;
+      }
+
+      if (
+        board.getBoard()[0][2] +
+          board.getBoard()[1][1] +
+          board.getBoard()[2][0] ===
+        winningMark
+      ) {
+        winner.player = currentPlayerTurn;
+        winner.cells = ["02", "11", "20"];
+        winner.message = `${currentPlayerTurn.getName()} is the winner!`;
+      }
+    }
+  }
 
   const getWinner = () => winner;
 
-  return { getWinner };
+  return { getWinner, updateGame };
 }
 
-const newGameButton = document.querySelector(".new-game-button");
-const boardDiv = document.querySelector(".board");
-
 newGameButton.addEventListener("click", () => {
+  resultDiv.innerText = null;
+  boardDiv.replaceChildren();
+  //Draw new board
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      const cell = document.createElement("div");
-      cell.id = `${i}${j}`;
-      cell.classList.add("cell");
-      boardDiv.appendChild(cell);
+      const newCell = document.createElement("div");
+      newCell.id = `${i}${j}`;
+      newCell.classList.add("cell");
+      boardDiv.appendChild(newCell);
     }
   }
+
+  const gameBoard = (function Gameboard() {
+    let board = [
+      [".", ".", "."],
+      [".", ".", "."],
+      [".", ".", "."],
+    ];
+    const getBoard = () => board;
+    const setBoard = (row, col, mark) => {
+      if (row < 3 && row >= 0 && col < 3 && col >= 0) board[row][col] = mark;
+    };
+    return { getBoard, setBoard };
+  })();
+
+  const game = createGame("player1", "player2", gameBoard);
+
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      game.updateGame(cell);
+    });
+  });
 });
